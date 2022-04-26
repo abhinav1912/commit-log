37bccd90a022f276f9c6690d5765179381430c9e
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-04-26 22:55:15 +0530

    refactor: using custom offset error

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -111,7 +110,7 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 		}
 	}
 	if s == nil || s.nextOffset <= off {
-		return nil, fmt.Errorf("Offset out of range: %d", off)
+		return nil, api.ErrorOffsetoOutOfRange{Offset: off}
 	}
 	return s.Read(off)
 }
